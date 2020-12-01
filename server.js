e1ebd6ac2da1fe297d2be61b4d3373136930529a
BuildTools <unconfigured@null.spigotmc.org>
2020-12-01 19:10:32 +0100

    Datenbank + initialisierungsdatei hinzugefügt. In server.js diese in Node eingebunden

--- server.js
+++ server.js
@@ -12,14 +12,14 @@ app.set('view engine', 'ejs');
 
 // Initialisiere Datenbankmodul sqlite3
 const sqlite3 = require('sqlite3').verbose();
-// TODO:
-/*
-let db = new sqlite3.Database('DATENBANK.db', (err) => {
- if (err) {
- console.error(err.message);
- }
- console.log('Connected to the shop database.');
- */
+
+
+let db = new sqlite3.Database('cinema.db', (err) => {
+if (err) {
+    console.error(err.message);
+}
+console.log('Connected to the shop database.');
+});
 
 // Initialisiere Modul cookie-parser
 const cookieParser = require('cookie-parser');
